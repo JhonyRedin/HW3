f6dc09331a7fb45ade87f8ef3e7e6bbce5715926
Georgy Redin <georgyredin@gmail.com>
2023-01-28 16:31:34 +0300

    Task3 result

---
+++ task23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+System.Console.WriteLine("Введите число больше 1: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+double exp = 0;
+
+if (num >0){
+for (int i=1; i <= num; i++)
+{
+    exp = Math.Pow (i,3);
+    System.Console.Write($"{exp} ");
+}
+}
+else
+{
+    System.Console.WriteLine("Введено не корректное число");
+}
