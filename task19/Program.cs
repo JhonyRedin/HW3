d3970fc541de95c1558f34d9dce90e4e0e0d8384
Georgy Redin <georgyredin@gmail.com>
2023-01-28 15:13:35 +0300

    First task result

---
+++ task19/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.WriteLine("Введите пятизначное чисо: ");
+string? text = Console.ReadLine();
+if (text!.Length == 5)
+{
+    if ( text [0] == text [4] && text[1] == text [3])
+    {
+        System.Console.WriteLine($"{text} -> полиндром");
+    }
+    else System.Console.WriteLine($"{text} -> не полиндром");
+}
+else System.Console.WriteLine("Введено неверное число");
